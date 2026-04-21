6ce991457316bf9f5e3c596b8a2cd8df84754b83
TechPanelGM <giomook2003@gmail.com>
2026-04-21 19:57:41 +0000

    oriole: Update blobs from BP4A.251205.006

---
+++ Android.mk
@@ -0,0 +1,19 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),oriole)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,244cd1019b43f25f4522b496608f5df08e1010f9)
+$(call add-radio-file-sha1-checked,radio/bl1.img,3029048d15bc679a93eccd49b3d3e873a862ad68)
+$(call add-radio-file-sha1-checked,radio/bl2.img,fdfc637610b5b73b7b86383a3b0d42b22c2ba8fc)
+$(call add-radio-file-sha1-checked,radio/bl31.img,fc0dadedadc348dd17a43e63c0d76a8d06183422)
+$(call add-radio-file-sha1-checked,radio/gsa.img,50a8ff362d0edf8fe71a9e3444b021f7c46ed241)
+$(call add-radio-file-sha1-checked,radio/ldfw.img,9cb45a64448975ebbe9862132f6c6711d152e83e)
+$(call add-radio-file-sha1-checked,radio/modem.img,714a4bcf40db4b91528be0ac7dd8942d62a51e68)
+$(call add-radio-file-sha1-checked,radio/pbl.img,741a203f0c033358c91eef177ed7ddd69aa63e42)
+$(call add-radio-file-sha1-checked,radio/tzsw.img,4c1e9622a523c03ab59a504d77de6864749d93fb)
+
+endif
